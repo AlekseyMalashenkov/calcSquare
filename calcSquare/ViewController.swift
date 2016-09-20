ddf481053dbd6c3328ef96c5f2d9fe91fecb4215
Demut <malashenkov93av@gmail.com>
2016-09-20 22:37:51 +0300

    change
    change

--- calcSquare/ViewController.swift
+++ calcSquare/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 0
+    
+    func calcResult (num : Int) -> Int {
+        var result = 0
+        var iterat = num
+        while (iterat != 0)  {
+            result += num
+            iterat -= 1
+        }
+        return result
+    }
 
+    @IBOutlet weak var enterNumberField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func calculate() {
+        
+        let tmp = Int(enterNumberField.text!)
+        
+        if tmp != nil {
+            let value = calcResult(num: tmp!)
+            resultLabel!.text = String(value)
+            count = count + 1
+            countLabel!.text = String(count)
+        } else {
+            resultLabel!.text = String("результат")
+        }
     }
 
-
+    @IBAction func refresh() {
+        count = 0
+        countLabel!.text = String("#")
+        resultLabel!.text = String("результат")
+    }
 }
 
